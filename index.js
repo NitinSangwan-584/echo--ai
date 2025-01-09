3821f80e1ae7225374e59e29632a8caf7bc5a2fa
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-01-09 10:00:40 +0530

    footer done

--- index.js
+++ index.js
@@ -5,25 +5,13 @@ const navbarbtn = () => {
     document.getElementById("rotats2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
-document.querySelectorAll('.accordion-item').forEach(item => {
-    const button = item.querySelector('.accordion-header');
-    const arrow = item.querySelector('.arrow');
-    const content = item.querySelector('.accordion-content');
 
-    button.addEventListener('click', () => {
-        // Toggle active class for content visibility
-        content.classList.toggle('active');
-
-        // Rotate the arrow
-        arrow.classList.toggle('rotated');
-
-        // Optionally, close other open items
-        document.querySelectorAll('.accordion-item').forEach(otherItem => {
-            if (otherItem !== item) {
-                otherItem.querySelector('.accordion-content').classList.remove('active');
-                otherItem.querySelector('.arrow').classList.remove('rotated');
-            }
-
-        });
+// // // accordian code
+const accordionButtons = document.querySelectorAll('.accordion-button');
+accordionButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        const activeAccordionItem = document.querySelector('.accordion-item.active');
+        const accordionItem = this.closest('.accordion-item');
+        accordionItem.classList.toggle('active') && activeAccordionItem.classList.remove("active")
     });
-});
+}); 
